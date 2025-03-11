a0946d5ba43660823dfdb9a62ad30d287264e154
Jose M. <20241548@itla.edu.do>
2025-03-11 14:25:49 -0400

    add: Constructor Overloading and POO

---
+++ Fundamentos/POO-Programacion-Orientada/Aritmetica.py
@@ -0,0 +1,63 @@
+'''
+Ejercicio: Creación de la Clase Aritmética
+
+- Definir la Clase Aritmética:
+   - Crear la clase Aritmetica
+   - Agregar dos atributos:
+     - ooperando1: un valor de tipo entero
+     - ooperando2: un valor de tipo entero
+- Agregar los siguientes métodos:
+     - sumar: suma los dos operandos
+     - restar: resta el segundo operando del primero
+     - multiplicar: multiplica los dos operandos
+     - dividir: divide el primer operando por el segundo, comprobando que el divisor no sea cero
+     '''
+     
+     
+class Aritmetica:
+    def __init__(self,operador1,operador2):
+        self.operando1 = operador1
+        self.operando2 = operador2
+        
+    def suma(self):
+                return self.operando1 + self.operando2
+            
+    def resta(self):
+                return self.operando1 - self.operando2
+            
+    def multiplica(self):
+                return self.operando1 * self.operando2
+            
+    def ponencia(self):
+                return self.operando1 ** self.operando2
+            
+    def divide(self):
+              if self.operando2 != 0:
+                 return self.operando1 / self.operando2
+              else:
+                 return "Error: División por cero"
+             
+             
+             
+if __name__ == '__main__': # Si tu estas aquí, se ejecuta, si no lo hace. The end 
+            
+    # Creación del primer objeto
+    aritmetica1 = Aritmetica(5, 3)
+
+    output = f'''
+    Suma: {aritmetica1.suma()}
+    Resta: {aritmetica1.resta()}
+    Muntiplicacion: {aritmetica1.multiplica()}
+    Divicion: {aritmetica1.divide()}
+    Pontenciacion: {aritmetica1.ponencia()}
+    '''
+
+    print(output)
+
+'''
+Explicacion teorica tecnica: 
+
+La construcción if __name__ == '__main__': 
+es muy útil para asegurar que cierto código solo se ejecute 
+cuando el script se ejecuta directamente,
+no cuando se importa como un módulo en otro script.'''
--- Fundamentos/POO-Programacion-Orientada/Persona-POO.py
+++ Fundamentos/POO-Programacion-Orientada/Persona-POO.py
@@ -1,21 +1,26 @@
-# Crea una clase persona que tenga los atributos nombre y edad 
-
 class Persona:
-    def __init__(self, nombre, edad):
+    def __init__(self, ID='000', nombre='SIN NOMBRE ASIGNADO', edad=0):   
+        self.ID = ID 
         self.nombre = nombre 
         self.edad = edad
         
+    @classmethod
+    def InicializarConNombreEdad(cls, nombre, edad):  # En teoria esto funcionaria como una SobreCarga del constructor 
+        return cls(nombre=nombre, edad=edad)
     
-        
     def MostrarLaInformacion(self):
-         print(f'''{self.nombre} - {self.edad}''')
+         print(f'ID: {self.ID} Nombre: {self.nombre} Edad: {self.edad}')
          
          
-Persona1 = Persona(nombre='Tomas Casas',edad=21)
-Persona2 = Persona(nombre='Ramon Taveras',edad=22)
-Persona3 = Persona(nombre='Mariano Taveras',edad=25)
+if __name__ == '__main__':
+         
+    # Crear instancias de la clase Persona
+    Persona1 = Persona(ID='001', nombre='Tomas Casas', edad=21)
+    Persona2 = Persona(ID='002',nombre='Mariano Taveras', edad=25)
+    persona3 = Persona()  # Constructor por defecto
+    persona4 = Persona.InicializarConNombreEdad('Ramon Emilio', 30)
 
-
-Persona1.MostrarLaInformacion()
-Persona2.MostrarLaInformacion()
-Persona3.MostrarLaInformacion()
+    Persona1.MostrarLaInformacion()
+    Persona2.MostrarLaInformacion() 
+    persona3.MostrarLaInformacion()
+    persona4.MostrarLaInformacion()
